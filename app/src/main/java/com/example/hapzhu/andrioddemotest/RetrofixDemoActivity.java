57863b015999fb69fd7280f7451a18d72f2f7b00
sivenzsw <sivenzsw@softspeed.com.cn>
2017-04-16 00:12:19 +0800

    retrofix demo update

--- app/src/main/java/com/example/hapzhu/andrioddemotest/RetrofixDemoActivity.java
+++ app/src/main/java/com/example/hapzhu/andrioddemotest/RetrofixDemoActivity.java
@@ -3,6 +3,7 @@ package com.example.hapzhu.andrioddemotest;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,17 +29,21 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -52,37 +57,33 @@ public class RetrofixDemoActivity extends AppCompatActivity implements LoaderCal
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
+    /**用于显示网络返回结果的控件*/
+    private TextView mResultTextView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
     private Retrofit mRetrofit;
-    public interface GitHubService {
-        @GET("users/{user}/repos")
-        Call<String> listRepos(@Path("user") String user);
+    public interface LoginService {
+        @POST("{filepath}/login")
+        Call<ResponseBody> login4Path(@Path("filepath") String filePath,
+                                      @Query("name")String name,
+                                      @Query("pwd")String pwd);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofix_demo);
+        mRetrofit=new Retrofit.Builder()
+                .baseUrl("http://192.168.0.199:8080/")
+                .build();
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
-        mRetrofit=new Retrofit.Builder().baseUrl("https://api.github.com/").build();
+        mResultTextView= (TextView) findViewById(R.id.tv_result_str);
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -157,9 +158,9 @@ public class RetrofixDemoActivity extends AppCompatActivity implements LoaderCal
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
+//        if (mAuthTask != null) {
+//            return;
+//        }
 
         // Reset errors.
         mEmailView.setError(null);
@@ -198,14 +199,51 @@ public class RetrofixDemoActivity extends AppCompatActivity implements LoaderCal
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            doLogin();
+//            mAuthTask = new UserLoginTask(email, password);
+//            mAuthTask.execute((Void) null);
+            //发送登录请求
         }
+    }
+
+    private void doLogin() {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        LoginService service=mRetrofit.create(LoginService.class);
+        Call<ResponseBody> call=service.login4Path("HttpRequestDemo",email,password);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                ResponseBody body=response.body();
+                String resultStr= "";
+                try {
+                    resultStr = body.string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                mResultTextView.setText(resultStr);
+                showProgress(false);
+                if(resultStr.contains("fail")){
+                    mPasswordView.requestFocus();
+                    Toast.makeText(getApplicationContext(),"登录失败",Toast.LENGTH_SHORT).show();
+                }else if(resultStr.contains("success")){
+                    startActivity(new Intent(RetrofixDemoActivity.this,LitepalDemoActivity.class));
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
     }
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return email.contains("qq");
     }
 
     private boolean isPasswordValid(String password) {
@@ -303,77 +341,5 @@ public class RetrofixDemoActivity extends AppCompatActivity implements LoaderCal
         int IS_PRIMARY = 1;
     }
 
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-            Call<String> call=mRetrofit.create(GitHubService.class).listRepos("abc");
-            call.enqueue(new Callback<String>() {
-                @Override
-                public void onResponse(Call<String> call, Response<String> response) {
-
-                }
-
-                @Override
-                public void onFailure(Call<String> call, Throwable t) {
-                    t.printStackTrace();
-                }
-            });
-            try {
-               Response<String> response= call.execute();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
-    }
 }
 
